e7dad0c6532d56af4357be3ad1fb8d1d4a855068
Hugh Perkins <hughperkins@gmail.com>
2016-08-28 14:15:49 +0100

    more indentation stuff to sync with master

--- src/native/jeigen.cpp
+++ src/native/jeigen.cpp
@@ -22,15 +22,15 @@ using namespace std;
 using namespace Eigen;
 
 /* use Map intead of this method
-   void valuesToMatrix( int rows, int cols, double *values, MatrixXd *pM ) {
-       int i = 0;
-       for( int c = 0; c < cols; c++ ) {
-           for ( int r = 0; r < rows; r++ ) {
-               (*pM)(r,c) = values[i];
-               i++;
-           }
+void valuesToMatrix( int rows, int cols, double *values, MatrixXd *pM ) {
+   int i = 0;
+   for( int c = 0; c < cols; c++ ) {
+       for ( int r = 0; r < rows; r++ ) {
+           (*pM)(r,c) = values[i];
+           i++;
        }
-   }*/
+   }
+}*/
 
 void matrixToValues( int rows, int cols, const MatrixXd *pM, double *values ) {
     int i = 0;
